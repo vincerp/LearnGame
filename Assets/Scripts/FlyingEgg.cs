b21cbefc493455c007d34b3ab0a8f0f00f22acd0
vincerp <vincependrell@gmail.com>
2014-01-25 17:50:02 +0100

    why u no commit ლ(ﾟдﾟ)ლ

---
+++ Assets/Scripts/FlyingEgg.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyingEgg : MonoBehaviour {
+
+	public Vector3 target = Vector3.zero;
+	public Vector3 rotation = new Vector3(10f, 10f, 90f);
+	public float speed = 0.5f;
+	private Transform _tr;
+
+	void Start(){
+		_tr = transform;
+	}
+
+	void Update () {
+		if(target == Vector3.zero) return;
+		_tr.position = Vector3.MoveTowards(_tr.position, target, speed);
+		_tr.Rotate(rotation);
+		if(_tr.position == target) Destroy(gameObject);
+	}
+
+	public void SetTarget(Vector3 tg){
+		target = tg;
+	}
+}
---
+++ Assets/Scripts/GameLogic/Caldron.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Caldron : MonoBehaviour {
+	
+	private float lastTimeEggPulled = 0f;
+	public float timeBetweenEggs = 1f;
+	public GameObject egg;
+	public float speed;
+
+	public Vector3 moveTo;
+
+	public int eggsPulled = 0;
+	public int eggsTotal = 20;
+
+	void OnTriggerStay (Collider col) {
+		if(col.tag != "Player") return;
+
+		if(Time.time <= lastTimeEggPulled+timeBetweenEggs || Collectable.collected <= 0) return;
+		GameObject newEgg = Instantiate(egg) as GameObject;
+		newEgg.SendMessage("SetTarget", transform.position+moveTo);
+		newEgg.transform.position = col.transform.position;
+		Collectable.collected--;
+		eggsPulled++;
+		lastTimeEggPulled = Time.time;
+		if(eggsPulled>= eggsTotal){
+			//TODO: something!
+		}
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(transform.position+moveTo, 0.5f);
+	}
+}
