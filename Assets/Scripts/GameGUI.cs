505d0f96e96d0ee9a2a5d6163f3d804e879813d6
vincerp <vincependrell@gmail.com>
2014-01-25 17:49:29 +0100

    and even more stuff

---
+++ Assets/Scripts/GameGUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameGUI : MonoBehaviour {
+
+	public GUISkin skin;
+	public int totalEggs = 20;
+	public Rect eggsPos;
+
+	void OnGUI(){
+		if(skin)GUI.skin = skin;
+		GUI.Label(eggsPos, ""+Collectable.collected+"/"+totalEggs);
+	}
+}
