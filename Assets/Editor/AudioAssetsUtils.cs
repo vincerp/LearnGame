b5abca3a2656e80eaa9de7f8c3137a58d5be73a7
vincerp <vincependrell@gmail.com>
2014-03-14 15:34:03 +0100

    Texture and Audio Compression. Game is 10x lighter.

---
+++ Assets/Editor/AudioAssetsUtils.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class AudioAssetsUtils {
+
+	[MenuItem("Assets/Audio/Set Audio Files as 2D")]
+	public static void SetAudioFilesAs2D(){
+		Debug.Log(Selection.activeObject.GetType());
+		Object[] audios = Selection.GetFiltered(typeof(AudioClip), SelectionMode.Assets);
+
+		int amount = 0;
+		foreach(AudioClip a in audios){
+			string path = AssetDatabase.GetAssetPath(a);
+			AudioImporter ai = AudioImporter.GetAtPath(path) as AudioImporter;
+
+			if(ai.threeD){
+				ai.threeD = false;
+				amount++;
+				AssetDatabase.ImportAsset(path);
+			}
+		}
+
+		Debug.Log(""+amount+" AudioClips converted to 2D.");
+	}
+	
+	[MenuItem("Assets/Audio/Set Audio Files as 3D")]
+	public static void SetAudioFilesAs3D(){
+		Debug.Log(Selection.activeObject.GetType());
+		Object[] audios = Selection.GetFiltered(typeof(AudioClip), SelectionMode.Assets);
+		
+		int amount = 0;
+		foreach(AudioClip a in audios){
+			string path = AssetDatabase.GetAssetPath(a);
+			AudioImporter ai = AudioImporter.GetAtPath(path) as AudioImporter;
+			
+			if(!ai.threeD){
+				ai.threeD = true;
+				amount++;
+				AssetDatabase.ImportAsset(path);
+			}
+		}
+		
+		Debug.Log(""+amount+" AudioClips converted to 3D.");
+	}
+	
+	[MenuItem("Assets/Audio/Set Audio Files Format to Compressed")]
+	public static void SetAudioFilesAsCompressed(){
+		Debug.Log(Selection.activeObject.GetType());
+		Object[] audios = Selection.GetFiltered(typeof(AudioClip), SelectionMode.Assets);
+		
+		int amount = 0;
+		foreach(AudioClip a in audios){
+			string path = AssetDatabase.GetAssetPath(a);
+			AudioImporter ai = AudioImporter.GetAtPath(path) as AudioImporter;
+			
+			if(ai.format == AudioImporterFormat.Native){
+				ai.format = AudioImporterFormat.Compressed;
+				amount++;
+				AssetDatabase.ImportAsset(path);
+			}
+		}
+		
+		Debug.Log(""+amount+" AudioClips' format changed to compressed.");
+	}
+	
+	[MenuItem("Assets/Audio/Set Audio Files Format to Native")]
+	public static void SetAudioFilesAsNative(){
+		Debug.Log(Selection.activeObject.GetType());
+		Object[] audios = Selection.GetFiltered(typeof(AudioClip), SelectionMode.Assets);
+		
+		int amount = 0;
+		foreach(AudioClip a in audios){
+			string path = AssetDatabase.GetAssetPath(a);
+			AudioImporter ai = AudioImporter.GetAtPath(path) as AudioImporter;
+			
+			if(ai.format == AudioImporterFormat.Compressed){
+				ai.format = AudioImporterFormat.Native;
+				amount++;
+				AssetDatabase.ImportAsset(path);
+			}
+		}
+		
+		Debug.Log(""+amount+" AudioClips' format changed to native.");
+	}
+}
---
+++ Assets/Editor/ScreenshotUtils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ScreenshotUtils {
+
+	//TODO: Verify if there is a folder before trying to export the image
+	[MenuItem("Other/Screenshot")]
+	public static void Screenshot(){
+		string _name = "Screens/Screen_"+System.DateTime.Now.ToShortDateString().Replace('/', '.')+"-"+System.DateTime.Now.ToShortTimeString().Replace(":", ".").Replace(" ", "")+".png";
+		Debug.Log("Saving screenshot " + _name);
+		Application.CaptureScreenshot(_name);
+	}
+
+}
